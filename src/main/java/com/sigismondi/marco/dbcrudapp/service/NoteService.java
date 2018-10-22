be5399f832868a2d22a5d16c09ed300692296c6f
msigismo <marco.sigismondi@capgemini.com>
2018-10-22 17:04:36 +0200

    Initialized app

---
+++ src/main/java/com/sigismondi/marco/dbcrudapp/service/NoteService.java
@@ -0,0 +1,23 @@
+package com.sigismondi.marco.dbcrudapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sigismondi.marco.dbcrudapp.entity.NoteEntity;
+import com.sigismondi.marco.dbcrudapp.repository.NoteRepository;
+
+@RestController
+public class NoteService {
+	
+	@Autowired
+	private NoteRepository noteRepository;
+	
+	@GetMapping("/notes")
+	public List<NoteEntity> retrieveAllStudents() {
+		return noteRepository.findAll();
+	}
+
+}
---
+++ src/test/java/com/sigismondi/marco/dbcrudapp/DbCrudAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sigismondi.marco.dbcrudapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbCrudAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

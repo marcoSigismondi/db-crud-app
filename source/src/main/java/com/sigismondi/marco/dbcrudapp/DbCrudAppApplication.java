4986b952f6e3b36d7be07ddd8f1035bf8916db2e
msigismo <marco.sigismondi@capgemini.com>
2018-11-02 16:59:24 +0100

    again improved structure

---
+++ source/src/main/java/com/sigismondi/marco/dbcrudapp/DbCrudAppApplication.java
@@ -0,0 +1,12 @@
+package com.sigismondi.marco.dbcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbCrudAppApplication.class, args);
+	}
+}
